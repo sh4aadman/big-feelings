13829d6ff552eed526ac3dc8e0fe0cbfa1a48322
sh4aadman <sh4adman@gmail.com>
2023-11-19 00:05:06 +0600

    added skeleton

---
+++ scripts/play-btn.js
@@ -0,0 +1,3 @@
+document.getElementById('play-btn').onclick('click', function() {
+    window.location.href = 'https://w3schools.invisionzone.com/topic/62282-how-to-windowlocationhref-target_blank/';
+})
